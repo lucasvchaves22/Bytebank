25bca2ccee5e1ed79ffd9ec5d1670214f1529928
Lucas Vasconcelos <lucasvchaves22@gmail.com>
2024-08-09 22:19:52 -0300

    Separando o codigo em arquivos de controle

--- src/nova-transacao-component.ts
+++ src/nova-transacao-component.ts
@@ -1,11 +1,3 @@
-let saldo = 3000;
-
-
-const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
-if (elementoSaldo != null) {
-    elementoSaldo.textContent = saldo.toString();
-}
-
 const elementoFormulario = document.querySelector(".block-nova-transacao form") as HTMLFormElement;
 elementoFormulario.addEventListener("submit", function(event) {
     event.preventDefault(); //previne que a página seja recarregada assim que der o submit no form
@@ -43,4 +35,5 @@ elementoFormulario.addEventListener("submit", function(event) {
     elementoFormulario.reset();
 
 });
+
 
---
+++ src/saldo-component.ts
@@ -0,0 +1,9 @@
+let saldo = 3000;
+alert("Testando");
+
+const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
+if (elementoSaldo != null) {
+    elementoSaldo.textContent = saldo.toString();
+}
+
+
