977d18fee78ceeca39cd2018f623cc5c8013280e
Lucas Vasconcelos <lucasvchaves22@gmail.com>
2024-08-10 11:13:43 -0300

    Adicionando Enum para formatos de datas.

--- src/saldo-component.ts
+++ src/saldo-component.ts
@@ -8,5 +8,5 @@ if (elementoSaldo != null) {
 
 if (elementoDataAcesso != null) {
     const dataAcesso: Date = new Date();
-    elementoDataAcesso.textContent = formatarData(dataAcesso);
+    elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO);
 }
